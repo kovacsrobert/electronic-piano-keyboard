759d281c81fba4c3d1da570af83bb4da495fd307
Róbert Kovács <robert.kovacs.centacc@gmail.com>
2017-09-06 21:51:25 +0200

    completed task #1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # electronic-piano-keyboard
 Arduino based electronic piano keyboard with preset songs
 
+## links
 Inspired by article https://www.hackster.io/lindsi8784/electronic-piano-keyboard-with-preset-songs-74ee7c
-
+Tinkercad circuit board: https://www.tinkercad.com/things/2mFQm6OSTCA-electronic-piano-keyboard
---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,18 @@
+int led = 13;
+int pianoKey1 = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  //pinMode(pianoKey1, INPUT);
+}
+
+void loop() {
+  int pianoKey1Value = analogRead(pianoKey1);
+  Serial.println(pianoKey1Value);
+  if (pianoKey1Value < 500) {
+    digitalWrite(led, LOW);
+  } else {
+    digitalWrite(led, HIGH);
+  }
+}
