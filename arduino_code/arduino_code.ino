0d220755c5ab08e094338dacb801843a3cbd4169
Róbert Kovács <robert.kovacs.centacc@gmail.com>
2017-09-30 19:44:52 +0200

    added piezo speaker code for the switch

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,18 +1,25 @@
-int led = 13;
-int pianoKey1 = 0;
+
+#include "notes.h"
+
+int led = LED_BUILTIN;
+int pianoPin1 = 0;
+int piezoPin = 8;
 
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-  //pinMode(pianoKey1, INPUT);
+  pinMode(pianoPin1, INPUT);
+  pinMode(piezoPin, OUTPUT);
 }
 
 void loop() {
-  int pianoKey1Value = analogRead(pianoKey1);
+  int pianoKey1Value = analogRead(pianoPin1);
   Serial.println(pianoKey1Value);
   if (pianoKey1Value < 500) {
     digitalWrite(led, LOW);
+    noTone(piezoPin);
   } else {
     digitalWrite(led, HIGH);
+    tone(piezoPin, NOTE_C4);
   }
 }
